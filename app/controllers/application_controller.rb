827672dc1b7292cef8e62ab684e080233fd2ec8e
villagebamboo@gmail.com <villagebamboo@gmail.com>
2024-05-28 21:38:55 +0900

    applicationコントローラーの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
 
+  #before_action :basic_auth,configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name,:family_name,:first_name,:family_name_kana,:first_name_kana,:birth_day])
+  end
 
 
   private
