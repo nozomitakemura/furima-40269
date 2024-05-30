2323bcd47a4db24eb39bc6aeb198616d3040c95b
villagebamboo@gmail.com <villagebamboo@gmail.com>
2024-05-31 00:41:07 +0900

    itemsテーブルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,10 @@
+class Item < ApplicationRecord
+
+belongs_to :user
+#has_one :order(orderモデル追加後記述)
+
+
+#validates :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_days_id, :price, presence: true
+#validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+end
