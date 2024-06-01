5edbcf0d2e0c6bbf249b1ccb2a6c856d68afaf48
villagebamboo@gmail.com <villagebamboo@gmail.com>
2024-06-01 21:00:56 +0900

    出品後の商品編集ページへのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root to: 'items#index'
-  resources :items, only: [:new, :index, :create, :show] 
+  resources :items, only: [:new, :index, :create, :show, :edit, :update] 
   end
 
 
