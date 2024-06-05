e3131663ad0e60875624c05bea99def3744f78b2
villagebamboo@gmail.com <villagebamboo@gmail.com>
2024-06-05 14:46:16 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root to: 'items#index'
-  resources :items, only: [:new, :index, :create, :show, :edit, :update, :destroy] 
+  # root to: 'items#index'
+  # resources :items, only: [:new, :index, :create, :show, :edit, :update, :destroy]  #購入機能実装前の記述
+
+devise_for :users  #合っているかわからない
+  root to: "items#index"
+  resources :users, only: [:edit, :update]
+  resources :items do
+    resources :orders, only: [:index, :create]
   end
-
-
-
+end
