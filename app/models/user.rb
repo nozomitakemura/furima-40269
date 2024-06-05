76765f6d14dc4ccd9795aaf45100216d9e17704e
villagebamboo@gmail.com <villagebamboo@gmail.com>
2024-06-05 14:47:17 +0900

    userテーブルへのアソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
-  # has_many :orders(orderモデル追加後記述)
+  has_many :orders
 
   validates :nick_name, presence: true
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合で入力してください' }
