2fb448a3f9f69ecf51fd8c0ad89ce3470a5bea52
villagebamboo@gmail.com <villagebamboo@gmail.com>
2024-06-05 15:42:31 +0900

    rubocopの実施

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,12 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item
-  before_action :redirect_if_not_owner, only: [:index,:create]
-  before_action :redirect_if_sold_out, only: [:index,:create]
+  before_action :redirect_if_not_owner, only: [:index, :create]
+  before_action :redirect_if_sold_out, only: [:index, :create]
 
- 
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address = OrderAddress.new
-    
   end
 
   def create
@@ -18,40 +16,41 @@ class OrdersController < ApplicationController
       @order_address.save(order_params, current_user.id)
       redirect_to root_path
     else
-      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
-end
+  end
 
-private
+  private
 
-def set_item
-  @item = Item.find(params[:item_id])
-end
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
-def order_params
-  params.require(:order_address).permit(:postcode, :prefecture_id, :municipalities, :block, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, price: @item.price, token: params[:token])
-end
+  def order_params
+    params.require(:order_address).permit(:postcode, :prefecture_id, :municipalities, :block, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: @item.id, price: @item.price, token: params[:token]
+    )
+  end
 
-def redirect_if_sold_out
-  if @item.purchased? 
+  def redirect_if_sold_out
+    return unless @item.purchased?
+
     redirect_to root_path
   end
-end
 
-def redirect_if_not_owner
-  if current_user == @item.user
+  def redirect_if_not_owner
+    return unless current_user == @item.user
+
     redirect_to root_path
   end
-end
 
-def pay_item
-  Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
-  Payjp::Charge.create(
-    amount: order_params[:price],
-    card: order_params[:token],
-    currency: 'jpy'
-  )
-end
-
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
